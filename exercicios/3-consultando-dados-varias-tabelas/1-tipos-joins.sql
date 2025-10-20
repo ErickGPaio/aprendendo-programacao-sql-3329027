6f2202fb0260445e05dde0ebd69ade6cecd82e44
Érick Gabriel Paio <105252121+ErickGPaio@users.noreply.github.com>
2025-10-20 20:56:03 +0000

    Joins

--- exercicios/3-consultando-dados-varias-tabelas/1-tipos-joins.sql
+++ exercicios/3-consultando-dados-varias-tabelas/1-tipos-joins.sql
@@ -1,1 +1,5 @@
 -- Crie uma consulta para realizar um left join com as tabelas "albums" e "artists"
+SELECT * 
+FROM ALBUMS A
+LEFT JOIN ARTISTS B ON A.ARTISTID = B.ARTISTID;
+
--- exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
+++ exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
@@ -1,4 +1,25 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists"
+SELECT T.TRACKID,
+       T.NAME   AS MUSICA,
+       AB.TITLE AS ALBUM,
+       A.NAME   AS ARTISTA
+FROM TRACKS        AS T
+INNER JOIN ALBUMS  AS AB ON T.ALBUMID = AB.ALBUMID
+INNER JOIN ARTISTS AS A  ON A.ARTISTID = AB.ARTISTID;
 
 
 -- Refatore a consulta anterior usando a cláusula "with" e verifique o total de músicas existente na base de terminado artista, ex: Caetano Veloso
+WITH MUSICA AS (
+
+SELECT T.TRACKID,
+       T.NAME   AS MUSICA,
+       AB.TITLE AS ALBUM,
+       A.NAME   AS ARTISTA
+FROM TRACKS        AS T
+INNER JOIN ALBUMS  AS AB ON T.ALBUMID = AB.ALBUMID
+INNER JOIN ARTISTS AS A  ON A.ARTISTID = AB.ARTISTID)
+
+SELECT ARTISTA,
+       COUNT(MUSICA) AS QTD_MUSICA
+  FROM MUSICA
+WHERE UPPER(ARTISTA) LIKE 'CAETANO%';
